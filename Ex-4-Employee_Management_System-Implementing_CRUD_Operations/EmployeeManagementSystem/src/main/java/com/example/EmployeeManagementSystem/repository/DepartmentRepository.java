c47deae9c9fd2a410922ff85982a877d787ac528
Syed Masood <125417020+syed-masood-pro@users.noreply.github.com>
2024-08-13 20:25:51 +0530

    Delete Ex-4-Employee_Management_System-Implementing_CRUD_Operations/EmployeeManagementSystem directory

--- Ex-4-Employee_Management_System-Implementing_CRUD_Operations/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeemanagementsystem.repository;
-
-import com.example.employeemanagementsystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    // Custom query method to find department by name
-    Department findByName(String name);
-}
